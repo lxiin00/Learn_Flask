71a9d8901a7a6366567974d3689057eac07fd90d
lxiin00 <shaolxiin2@163.com>
2019-09-04 23:34:55 +0800

    update flask_demo.py

--- Flask_demo/flask_demo.py
+++ Flask_demo/flask_demo.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello Flask!</h1>'
+# @app.route('/greet', defaults={'name': 'Programmer'})
+@app.route('/greet')
+@app.route('/greet/<name>')
+def hello(name='Programmer'):
+    return '<h1>Hello %s!</h1>' % name
 
 if __name__ == '__main__':
     app.run()
