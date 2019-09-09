421992b721b16ef1d636326cbd5779128c92aec8
lxiin00 <794164260@qq.com>
2019-09-09 23:44:13 +0800

    learn flask form for uploading file

--- Flask_demo/form/app.py
+++ Flask_demo/form/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, session, send_from_directory
 from forms import LoginForm, UploadForm
 from flask_wtf import FlaskForm
+import os
+import uuid
 
 app = Flask(__name__)
 
 app.secret_key = 'secret string'
 
-app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 # 设置上传文件的最大值（单位为字节byte）
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024# 设置上传文件的最大值（单位为字节byte）
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
 
 @app.route('/index')
 def index():
@@ -26,9 +29,34 @@ def bootstrap():
     form = LoginForm()
     return render_template('bootstrap.html', form=form)
 
+# 统一处理对所有上传的文件重新命名
+def random_filename(filename):
+    ext = os.path.splitext(filename)[1]
+    new_filename = uuid.uuid4().hex + ext
+    return new_filename
+
+@app.route('/uploads/<path:filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+@app.route('/uploaded-images')
+def show_images():
+    return render_template('uploaded.html')
+
+@app.errorhandler(413)
+def too_large(e):
+    return render_template('errors/413.html'), 413
+
 @app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
     form = UploadForm()
+    if form.validate_on_submit():
+        f = form.photo.data
+        filename = random_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        flash('上传成功！')
+        session['filenames'] = [filename]
+        return redirect(url_for('show_images'))
     return render_template('upload_file.html', form=form)
 
 
