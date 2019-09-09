9a2f4f012f1a22c5e147d374e6f12e5d949a7e16
lxiin00 <794164260@qq.com>
2019-09-09 13:10:47 +0800

    learn flask form test2

--- Flask_demo/form/app.py
+++ Flask_demo/form/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from forms import LoginForm
+from forms import LoginForm, UploadForm
 from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 
 app.secret_key = 'secret string'
+
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 # 设置上传文件的最大值（单位为字节byte）
 
 @app.route('/index')
 def index():
@@ -23,6 +25,11 @@ def basic():
 def bootstrap():
     form = LoginForm()
     return render_template('bootstrap.html', form=form)
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    return render_template('upload_file.html', form=form)
 
 
 if __name__ == '__main__':
--- Flask_demo/form/forms.py
+++ Flask_demo/form/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, ValidationError
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 
 class LoginForm(FlaskForm):
@@ -25,4 +26,23 @@ def is_42(form, field):
 
 class FortyTwoForm2(FlaskForm):
     answer = IntegerField('数字')
+    submit = SubmitField()
+
+
+# 工厂函数形式的全局验证器示例
+def is_42(message=None):
+    if message is None:
+        message = '必须为42！'
+
+    def _is_42(form, field):
+        if field.data != 42:
+            raise ValidationError(message)
+
+class FortyTwoForm3(FlaskForm):
+    answer = IntegerField('数字', validators=[is_42()])
+    submit = SubmitField()
+
+# 上传文件
+class UploadForm(FlaskForm):
+    photo = FileField('上传图片', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
     submit = SubmitField()
