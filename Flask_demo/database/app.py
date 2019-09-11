acdc9fc6d91c250eb94cb0c4f54aeda405911f54
lxiin00 <794164260@qq.com>
2019-09-11 12:48:53 +0800

    create the database

--- Flask_demo/database/app.py
+++ Flask_demo/database/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, flash, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 import os
 import click
 
@@ -7,6 +10,8 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:////' + os.path.join(app.root_path, 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'secret string'  # app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
+
 
 db = SQLAlchemy(app)
 
@@ -19,6 +24,34 @@ def initdb():
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Note %r>' % self.body
+
+class NewNoteForm(FlaskForm):
+    body = TextAreaField('内容', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+class DeleteNoteForm(FlaskForm):
+    submit = SubmitField('删除')
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_note():
+    form = NewNoteForm()
+    if form.validate_on_submit():
+        body = form.body.data
+        note = Note(body=body)
+        db.session.add(note)
+        db.session.commit()
+        flash('Note已经保存成功！')
+        return redirect(url_for('index'))
+    return render_template('new_note.html', form=form)
+
+@app.route('/index')
+def index():
+    form = DeleteNoteForm()
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes, form=form)
 
 
 if __name__ == '__main__':
