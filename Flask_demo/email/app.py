1240d0d64de4af8694585cff166fa139d2bf03e2
lxiin00 <794164260@qq.com>
2019-09-16 23:43:32 +0800

    learn Flask email

--- Flask_demo/email/app.py
+++ Flask_demo/email/app.py
@@ -3,6 +3,7 @@ from flask_mail import Mail, Message
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
+from threading import Thread
 import os
 
 app = Flask(__name__)
@@ -12,13 +13,13 @@ app.jinja_env.lstrip_blocks = True
 app.config.update(
     SECRET_KEY = os.getenv('SECRET_KEY', 'secret string'),
     # MAIL_SERVER = os.getenv('MAIL_SERVER'),
-    MAIL_SERVER = 'smtp.163.com',
+    MAIL_SERVER = 'smtp.qq.com',
     MAIL_PORT = 465,
     MAIL_USE_SSL = True,
     # MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
     MAIL_USERNAME = 'xxx',
     # MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
-    MAIL_PASSWORD = 'xxx',
+    MAIL_PASSWORD = 'xxxx',
     # MAIL_DEFAULT_SENDER = ('Lxiin00', os.getenv('MAIL_USERNAME'))
     MAIL_DEFAULT_SENDER = ('Lxiin00', 'xxx')
 )
@@ -29,10 +30,21 @@ def send_smtp_mail(subject, to, body):
     message = Message(subject, recipients=[to], body=body)
     mail.send(message)
 
+def _send_async_mail(app, message):
+    with app.app_context():
+        mail.send(message)
 
 def send_async_email(subject, to, body):
-    pass
+    message = Message(subject, recipients=[to], body=body)
+    thr = Thread(target=_send_async_mail, args=[app, message])
+    thr.start()
+    return thr
 
+def send_subscribe_mail(subject, to, **kwargs):
+    message = Message(subject, recipients=[to], sender='XXX')
+    message.body = render_template('emails/subscribe.txt', **kwargs)
+    message.html = render_template('emails/subscribe.html', **kwargs)
+    mail.send(message)
 
 class SubscribeForm(FlaskForm):
     name = StringField('姓名', validators=[DataRequired()])
@@ -73,11 +85,15 @@ def subscribe():
         name = form.name.data
         email = form.email.data
         flash('发送成功！')
-        send_smtp_mail('订阅成功！', email, '您好，感谢您的订阅!')
+        # send_smtp_mail('订阅成功！', email, '您好，感谢您的订阅!')
+        send_subscribe_mail('订阅成功！', email, name=name)
         return redirect(url_for('index'))
     return render_template('subscribe.html', form=form)
 
-
+@app.route('/unsubscribe')
+def unsubscribe():
+    flash('想取消订阅？！没门！')
+    return redirect(url_for('subscribe'))
 
 if __name__ == '__main__':
     app.run(debug=True)
